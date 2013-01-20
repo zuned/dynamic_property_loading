38363a5c0b34e867ab58f5e65ec17a7a54204bbc
Zuned Ahmed <zuned485@yahoo.com>
2013-01-20 20:41:09 +0530

    Revert "loading properties from database and injecting to bean"
    This reverts commit 6b74a68c2d298d64b6e8ea5f5d125c2d9e914815.

--- loading property from db/src/main/java/net/pureessence/example/Constants.java
+++
@@ -1,7 +0,0 @@
-package net.pureessence.example;
-
-
-public abstract class Constants {
-    public static final String APPLICATION_ONE = "ApplicationOne";
-    public static final String APPLICATION_TWO = "ApplicationTwo";
-}
