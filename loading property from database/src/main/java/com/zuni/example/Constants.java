0acc518a27fbd9e3a073920fc36d02df36e24eef
Zuned Ahmed <zuned485@yahoo.com>
2013-01-20 20:43:36 +0530

    loading database from DB and injecting in autowired bean

---
+++ loading property from database/src/main/java/com/zuni/example/Constants.java
@@ -0,0 +1,7 @@
+package com.zuni.example;
+
+
+public abstract class Constants {
+    public static final String APPLICATION_ONE = "ApplicationOne";
+    public static final String APPLICATION_TWO = "ApplicationTwo";
+}
